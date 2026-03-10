60902b9215dae5a6e016e3bc7cc008f96c237052
bcthaniel10 <pablo.oliveira10@escola.pr.gov.br>
2026-03-10 11:41:26 -0300

    Add files via upload

---
+++ endereco.js
@@ -0,0 +1,8 @@
+console.log("endereço completo");
+let rua = "rua das flores";
+let numero = 666;
+let cidade = "curitiba";
+
+let endereco = rua + "," + numero + "-" + cidade;
+console.log(endereco);
+console.log("o endereço correto é:" + endereco);
