f0c7985e3e840a405a4d7a769ff6ec603e3fa3d7
bcthaniel10 <pablo.oliveira10@escola.pr.gov.br>
2026-04-06 13:53:55 +0000

    v1

---
+++ saudacao.js
@@ -0,0 +1,23 @@
+//declaração da função com parâmetro
+function saudacao(nomeJogador) {
+    console.log("Bem vindo ao jogo, " + nomeJogador + "!")
+};
+
+saudacao("alerandro");
+
+//função que recebe um valor, soma  1 número e retorna
+function subirDeNivel(nivelAtual) {
+    return nivelAtual + "!";
+}
+
+let meuNivel = 10;
+
+let nivelAtualizado = subirDeNivel(meuNivel);
+console.log("seu nivel é: " + nivelAtualizado);
+
+//função que soma dois números
+function soma(numero1, numero2) {
+    return numero1 + numero2
+};
+
+console.log(soma(5,3));
